65833fa786dd307f32373cdd98af85edd593eb30
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-06-22 19:06:31 -0500

    DOM - obtener texto de un input y agregarlo/eliminarlo dinamicamente dentro de la pagina.

--- code/javascript/dom/taller_04/script.js
+++ code/javascript/dom/taller_04/script.js
@@ -1,7 +1,8 @@
 function cargar_elemento() {
     var container = document.getElementById('container')
+    var mensaje = document.getElementById('mensaje')
     var h1 = document.createElement('h1')
-    var texto = document.createTextNode('Guillermo Pizarro')
+    var texto = document.createTextNode( mensaje.value )
     h1.appendChild(texto)
     container.appendChild(h1)
 }
