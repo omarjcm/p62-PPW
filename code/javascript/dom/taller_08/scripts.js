2efcc48623ed5c6f1402e16690e77c49fda2e1c7
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-06-29 18:36:18 -0500

    Animacion - Implementacion del evento load.

---
+++ code/javascript/dom/taller_08/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    const cajita = document.getElementById('cajita')
+    const ctx = cajita.getContext('2d')
+    cajita.width = 500
+    cajita.height = 500
+})
