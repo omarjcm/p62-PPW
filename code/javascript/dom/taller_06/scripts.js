4b348d4846fb492e61e6a989393822d5baa0158d
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-06-28 21:19:36 -0500

    Eventos en mouse terminado.

--- code/javascript/dom/taller_06/scripts.js
+++ code/javascript/dom/taller_06/scripts.js
@@ -5,7 +5,10 @@ var offset = [0, 0]
 div_cajita.addEventListener('mousedown', function(valor){
     es_presionado = true
 
-    offset = [ div_cajita.offsetLeft - valor.clientX, div_cajita.offsetTop - valor.clientY ]
+    offset = [ 
+        div_cajita.offsetLeft - valor.clientX, 
+        div_cajita.offsetTop - valor.clientY 
+    ]
 }, true)
 
 div_cajita.addEventListener('mouseup', function(valor){
@@ -17,7 +20,7 @@ div_cajita.addEventListener('mouseup', function(valor){
 div_cajita.addEventListener('mousemove', function(valor){
     valor.preventDefault()
     if (es_presionado) {
-        div_cajita.style.top = (valor.clientX + offset[0]) + 'px'
-        div_cajita.style.left = (valor.clientY + offset[1]) + 'px'
+        div_cajita.style.left = (valor.clientX + offset[0]) + 'px'
+        div_cajita.style.top = (valor.clientY + offset[1]) + 'px'
     }
 }, true)
