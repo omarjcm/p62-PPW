0ea5d2e05b805a7c0f97d6df08b86336a3e1bddc
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-07-26 21:24:11 -0500

    Javascript Basico

---
+++ code/javascript/basic/taller_03/script.js
@@ -0,0 +1,11 @@
+var texto = 'Universidad Politecnica Salesiana'
+
+function imprimir() {
+    let texto2 = 'Programacion y Plataformas Web'
+    console.log(texto2)
+    console.log(texto)
+}
+
+console.log(texto)
+
+console.log(texto2)
