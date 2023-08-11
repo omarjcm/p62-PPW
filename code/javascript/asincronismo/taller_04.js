35c384aad594baf2c68ebeea30e6506ad01e2689
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-10 19:58:33 -0500

    Promesas

--- code/javascript/asincronismo/taller_04.js
+++ code/javascript/asincronismo/taller_04.js
@@ -7,6 +7,16 @@ const saludar = (nombre) => {
     })
 }
 
+const hablar = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Bla, bla, bla...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
 console.log('Iniciando conversacion...')
 saludar('Guillermo')
+    .then( (dato) => hablar( dato ) )
     .then( (dato) => console.log( dato ) )
