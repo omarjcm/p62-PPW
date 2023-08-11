4dbeec2b2ddf676b6b00b46d8acf8572d629c1bc
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-10 20:05:45 -0500

    Async/Await

---
+++ code/javascript/asincronismo/taller_05.js
@@ -0,0 +1,35 @@
+async function saludar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Hola ${nombre}.`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Bla, bla, bla...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Terminando conversacion...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
+
+async function main() {
+    console.log('Iniciando conversacion...')
+    let nombre = await saludar('Guillermo')
+    await hablar( nombre )
+    await adios( nombre ) 
+}
+
+main()
