0ccbc6604b3566283da7536cc4db91586d6b28f2
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-10 19:45:58 -0500

    Gestion del asincronismo.

--- code/javascript/asincronismo/taller_03.js
+++ code/javascript/asincronismo/taller_03.js
@@ -1,15 +1,23 @@
 function saludar(nombre, fn) {
     setTimeout(function() {
         console.log(`Hola ${nombre}`)
-    }, 1000)
+        fn(nombre, adios)
+    }, 3000)
 }
 
 function hablar(nombre, fn) {
     setTimeout(function(){
-        
+        console.log('bla, bla, bla...')
+        fn(nombre)
+    }, 1000)
+}
+
+function adios(nombre) {
+    setTimeout(function(){
+        console.log(`Adios ${nombre}.`)
+        console.log('Terminando conversacion...')
     }, 1000)
 }
 
 console.log('Iniciando conversacion...')
 saludar('Guillermo', hablar)
-console.log('Terminando conversacion...')
