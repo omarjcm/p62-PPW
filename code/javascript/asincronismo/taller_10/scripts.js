b94b25ce92b6a25f39fa7cba3b88c5b847d8ee24
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-17 17:39:08 -0500

    Gestion de asincronismo presentando informacion en HTML.

---
+++ code/javascript/asincronismo/taller_10/scripts.js
@@ -0,0 +1,38 @@
+const API = 'https://rickandmortyapi.com/api/character/'
+
+function personaje(texto) {
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode(texto.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_texto)
+    div.appendChild(h1)
+    let img = document.createElement('img')
+    img.src = texto.image
+    div.appendChild(img)
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( div )
+}
+
+function obtener_personaje(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`${API}${id}`)
+            .then((data) => resolve(data.json()))
+            .catch((error) => reject(`[error]: ${error}`))
+    })
+}
+
+let ids = []
+for (let i=1; i<=100; i++) {
+    ids.push(i)
+}
+
+let promesas = ids.map( id => obtener_personaje(id) )
+
+Promise
+    .all( promesas )
+    .then( data => {
+        for (let i=0; i<data.length; i++) {
+            personaje( data[i] )
+        }
+    } )
