65d55a8772e0c2559b5e351619f220ae456b6d5f
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-24 16:44:34 -0500

    Implementacion del solicitudes tipo GET y POST.

---
+++ code/javascript/nodejs/intermediate/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+var app = express()
+app.use( router )
+
+router.get('/', function(req, res) {
+    res.send('Hola desde GET.')
+})
+router.post('/', function(req, res) {
+    res.send('Hola desde POST.')
+})
+
+let puerto = 3000
+app.listen(puerto)
+console.log(`La aplicacion se encuentra arriba en http://localhost:${puerto}`)
