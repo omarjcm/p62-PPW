b7923dad76f7e4c92b57ed5dbe52fd5343ada09d
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-24 19:44:01 -0500

    Trabajando con QUERY y BODY.

--- code/nodejs/taller_01/index.js
+++ code/nodejs/taller_01/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const router = express.Router()
-
 var app = express()
+
+const router = express.Router()
 
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded({extended:false}) )
@@ -24,7 +24,15 @@ router.post('/', function(req, res) {
 router.delete('/mensaje', function(req, res) {
     console.log( req.query )
     console.log( req.body )
-    res.send( 'Mensaje eliminado correctamente. ' + req.body.text )
+    res.send( 'Mensaje eliminado correctamente. ' + req.body.texto1 + ' ' + req.body.texto2 )
+})
+
+router.put('/', function(req, res) {
+    res.send('Hola desde PUT.')
+})
+
+router.patch('/', function(req, res) {
+    res.send('Hola desde PATCH.')
 })
 
 let puerto = 3000
