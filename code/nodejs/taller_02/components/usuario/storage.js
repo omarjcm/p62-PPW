91a6a2378fea9ba40c45768393de02f8e01bfd41
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-08-30 20:41:15 -0500

    Obtener Usuario.

--- code/nodejs/taller_02/components/usuario/storage.js
+++ code/nodejs/taller_02/components/usuario/storage.js
@@ -1,10 +1,22 @@
 const Model = require('./model')
 
-function agregarUsuario( dato ) {
-    const resultado = new Model( dato )
+async function agregarUsuario( dato ) {
+    const resultado = await new Model( dato )
     return resultado.save()
+}
+
+async function obtenerUsuario( filtro ) {
+    let mi_filtro = {}
+
+    if (filtro.nombre != null) {
+        mi_filtro = { nombre: filtro.nombre }
+    }
+    const resultado = await Model.find( mi_filtro )
+    console.log(resultado)
+    return resultado
 }
 
 module.exports = {
     agregar:agregarUsuario,
+    obtener:obtenerUsuario,
 }
--- code/nodejs/taller_02/config.js
+++ code/nodejs/taller_02/config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 3000,
-    DB_URL: 'mongodb+srv://gpizarro:inexcelsisdeo@cluster0.turgu5m.mongodb.net/?retryWrites=true&w=majority'
+    DB_URL: 'mongodb://dba:dba@mongodb:27017/miapp?authSource=admin'
 }
 
 module.exports = config
